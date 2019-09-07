bf95ad3cb9b5a63aa0bbacbf4c9a6d53bfe599e1
Arthur <awimberly@surgefoward.com>
2019-09-07 00:44:43 -0500

    first commit for ZipIt .Net Core project

---
+++ Models/City.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace ZipIt.Models
+{
+    public class City
+    {
+        [Key]
+        public string Zip { get; set; }
+        public float Lat { get; set; }
+        public float Lng { get; set; }
+        public string CityName { get; set; }
+        public string StateId { get; set; }
+        public string StateName { get; set; }
+        public string TimeZone { get; set; }
+        public ICollection<City> Cities { get; set; }
+    }
+}
---
+++ Models/CityContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ZipIt.Models
+{
+    public class CityContext : DbContext
+    {
+        public CityContext(DbContextOptions<CityContext> options) : base(options) { }
+
+        public DbSet<City> Cities { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data Source=Database/cities.db"); // windows      
+        }
+    }
+}
